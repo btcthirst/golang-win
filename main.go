813d33876ba3063764c3b4af6d072601dd6bef26
Max Slobodianyk <btcthirst@gmail.com>
2021-02-10 09:54:56 +0200

    try variables and declarations

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var hello string
+	a, b, c := 5, 2.5, "some"
+
+	_, err := fmt.Scanln(&hello)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(hello)
+	fmt.Println(a, b, c)
+	fmt.Println(a + int(b))
+	fmt.Println(b + float64(a))
+	fmt.Println("hello World! and ", hello)
 	fmt.Println("hello World!")
 }
